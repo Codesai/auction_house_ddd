8bafc4a18abaf9078b7ab024c7a917fb5a9da1ca
Alfredo Casado <casado.alfredo@gmail.com>
2020-01-18 11:53:28 +0100

    Add verification for response type

--- src/main/java/com/codesai/auction_house/infrastructure/AuctionHouseAPI.java
+++ src/main/java/com/codesai/auction_house/infrastructure/AuctionHouseAPI.java
@@ -9,6 +9,7 @@ public class AuctionHouseAPI {
     public static Object createAuction(Request request, Response response) {
         response.status(201);
         response.header("Location", "http://localhost:9001/api/auction/" + UUID.randomUUID());
+        response.type("application/json");
         return "OK";
     }
 }
