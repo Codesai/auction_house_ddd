a7c5bacf06d95eb657491f4fa249f2fe6fbe4604
Miguel Ángel Viera <mangelviera93@gmail.com>
2020-01-23 01:15:25 +0100

    refactor: remove ifs and simplify code

--- src/main/java/com/codesai/auction_house/infrastructure/Routing.java
+++ src/main/java/com/codesai/auction_house/infrastructure/Routing.java
@@ -2,6 +2,8 @@ package com.codesai.auction_house.infrastructure;
 
 import spark.Spark;
 
+import java.util.Optional;
+
 import static spark.Spark.*;
 
 public class Routing {
@@ -21,25 +23,13 @@ public class Routing {
             response.header("Access-Control-Allow-Origin", "*");
         });
 
-        options("/*",
-                (request, response) -> {
-
-                    String accessControlRequestHeaders = request
-                            .headers("Access-Control-Request-Headers");
-                    if (accessControlRequestHeaders != null) {
-                        response.header("Access-Control-Allow-Headers",
-                                accessControlRequestHeaders);
-                    }
-
-                    String accessControlRequestMethod = request
-                            .headers("Access-Control-Request-Method");
-                    if (accessControlRequestMethod != null) {
-                        response.header("Access-Control-Allow-Methods",
-                                accessControlRequestMethod);
-                    }
-
-                    return "OK";
-                });
+        options("/*", (request, response) -> {
+            var accessControlRequestHeaders = Optional.ofNullable(request.headers("Access-Control-Request-Headers"));
+            accessControlRequestHeaders.ifPresent(c -> response.header("Access-Control-Allow-Headers", c));
+            var accessControlRequestMethod = Optional.ofNullable(request.headers("Access-Control-Request-Method"));
+            accessControlRequestMethod.ifPresent(c -> response.header("Access-Control-Allow-Methods", c));
+            return "OK";
+        });
 
 
     }
