0ec181e10f165eb914d479f8e595a2736ffa178e
Alfredo Casado <casado.alfredo@gmail.com>
2020-01-25 15:16:06 +0100

    create ConquerAuctionAction

--- src/main/java/com/codesai/auction_house/business/model/auction/Auction.java
+++ src/main/java/com/codesai/auction_house/business/model/auction/Auction.java
@@ -29,6 +29,7 @@ public class Auction {
     public Money minimumOverbiddingPrice;
     public List<Bid> bids;
 
+    // todo: test
     public Auction(Item item, Bid initialBid, Money conquerPrice, LocalDate expirationDate, Money minimumOverbiddingPrice) {
         if (conquerPrice.isLessThan(initialBid.money)) throw new InitialBidIsGreaterThanConquerPrice();
         if (minimumOverbiddingPrice.isLessThan(MINIMUM_MONEY_TO_OVERBID)) throw new MinimumOverbiddingPriceIsNotAllowed();
