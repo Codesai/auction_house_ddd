241c1e3d37d7e1a9803d69d447b73aaa3ead74c2
Alfredo Casado <alfredo@codesai.com>
2020-02-04 10:50:24 +0100

    remove things from todo

--- src/main/java/com/codesai/auction_house/App.java
+++ src/main/java/com/codesai/auction_house/App.java
@@ -2,8 +2,6 @@ package com.codesai.auction_house;
 
 import static com.codesai.auction_house.infrastructure.Routing.Routes;
 
-// TODO: Hacer definición swagger para conquer
-// TODO: Test api de conquer
 // TODO: Meter los usuarios
 // TODO: Proceso finalización del auction
 // TODO: Hacer algo para enseñar domain events
