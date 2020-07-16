7b6c8459d272d0c0b50c565e683c02b483e1aa6c
Miguel Ángel Viera <mangelviera93@gmail.com>
2020-07-16 15:34:24 +0200

    Set port at the same value again

--- src/main/java/com/codesai/auction_house/infrastructure/delivery_mechanism/Routing.java
+++ src/main/java/com/codesai/auction_house/infrastructure/delivery_mechanism/Routing.java
@@ -8,7 +8,7 @@ import static spark.Spark.*;
 
 public class Routing {
 
-    public final static Integer PORT = 8080;
+    public final static Integer PORT = 9001;
 
     public static void startApi() {
         System.out.println("starting auction house API: " + PORT);
