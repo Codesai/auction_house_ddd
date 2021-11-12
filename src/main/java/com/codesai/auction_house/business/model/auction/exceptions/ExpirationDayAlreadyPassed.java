b8046d4ebeef54c0abc9222bea898bf5f5a22dbc
Alfredo Casado <alfredo@codesai.com>
2021-11-12 11:52:01 +0100

    Exception inherits from AuctionException

--- src/main/java/com/codesai/auction_house/business/model/auction/exceptions/ExpirationDayAlreadyPassed.java
+++ src/main/java/com/codesai/auction_house/business/model/auction/exceptions/ExpirationDayAlreadyPassed.java
@@ -1,4 +1,7 @@
 package com.codesai.auction_house.business.model.auction.exceptions;
 
-public class ExpirationDayAlreadyPassed extends RuntimeException{
+public class ExpirationDayAlreadyPassed extends AuctionException {
+    public ExpirationDayAlreadyPassed() {
+        super("expiration date already passed");
+    }
 }
